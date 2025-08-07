2be6aa2eaac682e23ecc2d81164d0096be1458a4
Malia Cortez <mookiethompson17@gmail.com>
2025-08-06 17:23:29 -0700

    remove unneeded comments

--- app.R
+++ app.R
@@ -45,95 +45,6 @@ data$Short.Sleep.Duration <- as.numeric(data$Short.Sleep.Duration)
 df_vars <- data %>% 
   dplyr::select(c(2:100))
 
-# column_mapping <- c("PM2.5" = "Particulate.Matter.2.5",
-#          "Green space" = "Green.Space",
-#          "Nighttime radiance" = "Nighttime.Radiance",
-#          "Received SNAP Benefits" = "Food.Stamps",
-#          "Food insecurity" = "Food.Insecurity",
-#          "Housing insecurity" = "Housing.Insecurity",
-#          "Utility services threat" = "Utility.Services.Threat",
-#          "Lack of reliable transportation" = "Lacking.Reliable.Transportation",
-#          "Lack of social and emotional support" =  "Lack.of.Social.and.Emotional.Support",
-#          "Lack of health insurance" = "Lack.of.Health.Insurance",
-#          "Routine checkup in past year" = "Routine.Checkup.in.the.Past.Year",
-#          "Visited dentist in past year" = "Visited.Denstist.in.Past.Year",
-#          "Taking blood pressure medication" = "Taking.Medicine.to.Control.High.Blood.Pressure",
-#          "Cholesterol screening" = "Cholesterol.Screening",
-#          "Mammography screening for breast cancer" = "Mammography.Use.among.Women.50.to.74",
-#          "Colorectal cancer screening" = "Colorectal.Cancer.Screening.among.Adults.45.to.75",
-#          "Binge drinking" =  "Binge.Drinking.among.Adults",
-#          "Cigarette smoking" = "Cigarette.Smoking.among.Adults",
-#          "No physical activity" = "No.Leisure.time.Physical.Activity.among.Adults",
-#          "Short sleep duration" =  "Short.Sleep.Duration",
-#          "Arthritis" = "Arthritis.among.Adults",
-#          "Asthma" = "Asthma.among.Adults",
-#          "High blood pressure" = "High.Blood.Pressure.among.Adults",
-#          "Cancer" = "Cancer.or.Melanoma.among.Adults",
-#          "High cholesterol" = "High.Cholesterol.among.Screened.Adults",
-#          "Chronic obstructive pulmonary disease" = "COPD.among.Adults",
-#          "Coronary heart disease" = "Coronary.Heart.Disease.among.Adults",
-#          "Depression" = "Depression.among.Adults",
-#          "Diabetes" = "Diagnosed.Diabetes.among.Adults",
-#          "Obesity" = "Obesity.among.Adults",
-#          "All teeth lost" = "All.Teeth.Lost.among.Adults.65.and.Older",
-#          "Stroke" = "Stroke.among.Adults",
-#          "Cholesterol screening" = "Cholesterol.Screening",
-#          "Non-Hispanic White (percentage)" = "Percent.White.NonHispanic",
-#          "Non-Hispanic Black (percentage)" = "Percent.Black.NonHispanic",
-#          "Non-Hispanic Asian (percentage)" = "Percent.Asian.NonHispanic",
-#          "Non-Hispanic American Indian or Alaska Native (percentage)" = "Percent.American.Indian.Alaska.Native.NonHispanic",
-#          "Non-Hispanic Native Hawaiian or Pacific Islander (percentage)" =  "Percent.Native.Hawaiian.Pacific.Islander.NonHispanic",
-#          "Non-Hispanic Other (percentage)" = "Percent.Other.Race.NonHispanic",
-#          "Non-Hispanic two or more races (percentage)" = "Percent.Two.or.More.Races.NonHispanic",
-#          "Hispanic or Latino (percentage)" = "Percent.Hispanic.or.Latino",
-#          "Hispanic or Latino White (percentage)" = "Percent.White.Hispanic.or.Latino",
-#          "Hispanic or Latino Black (percentage)" = "Percent.Black.Hispanic.or.Latino",
-#          "Hispanic or Latino Asian (percentage)" = "Percent.Asian.Hispanic.or.Latino",
-#          "Hispanic or Latino American Indian or Alaska Native (percentage)" = "Percent.American.Indian.Alaska.Native.Hispanic.or.Latino",
-#          "Hispanic or Latino Native Hawaiian or Pacific Islander (percentage)" = "Percent.Native.Hawaiian.Pacific.Islander.Hispanic.or.Latino",
-#          "Hispanic or Latino Other (percentage)" = "Percent.Other.Race.Hispanic.or.Latino",
-#          "Hispanic or Latino two or more races (percentage)" = "Percent.Two.or.More.Races.Hispanic.or.Latino",
-#          "Total population" = "Total.Population",
-#          "Male (percentage)" = "Percent.Male",
-#          "Female (percentage)" = "Percent.Female",
-#          "0-4 years (percentage)" = "Percent.0.to.4.years",
-#          "5-9 years (percentage)" = "Percent.5.to.9.years",
-#          "10-14 years (percentage)" = "Percent.10.to.14.years",
-#          "15-19 years (percentage)" = "Percent.15.to.19.years",
-#          "20-24 years (percentage)" = "Percent.20.to.24.years",
-#          "25-29 years (percentage)" = "Percent.25.to.29.years",
-#          "30-34 years (percentage)" = "Percent.30.to.34.years",
-#          "35-39 years (percentage)" = "Percent.35.to.39.years",
-#          "40-44 years (percentage)" = "Percent.40.to.44.years",
-#          "45-49 years (percentage)" = "Percent.45.to.49.years",
-#          "50-54 years (percentage)" = "Percent.50.to.54.years",
-#          "55-59 years (percentage)" = "Percent.55.to.59.years",
-#          "60-64 years (percentage)" = "Percent.60.to.64.years",
-#          "65-69 years (percentage)" = "Percent.65.to.69.years",
-#          "70-74 years (percentage)" = "Percent.70.to.74.years",
-#          "75-79 years (percentage)" = "Percent.75.to.79.years",
-#          "80-84 years (percentage)" = "Percent.80.to.84.years",
-#          "85 years+ (percentage)" = "Percent.85.and.older",
-#          "Social Vulnerability Index" = "Social.Vulnerability.Index",
-#          "Environmental Justice Index" = "Environmental.Justice.Index",
-#          "UV index" = "UV.Index",
-#          "Pesticide exposure" = "Pesticide.Exposure",
-#          "Segregation" = "Racial.Residential.Segregation",
-#          "Noise pollution" = "Noise.Pollution",
-#          "No internet" = "No.broadband.internet",
-#          "No high school education" = "No.high.school.diploma",
-#          "Single parent households" = "Single.parent.households",
-#          "Housing cost burden" = "Housing.cost.burden",
-#          "Dew point" = "Dew.point",
-#          "Maximum temperature" = "Maximum.temperature",
-#          "Minimum temperature" = "Minimum.temperature",
-#          "Average temperature" = "Average.temperature",
-#          "Wildfire smoke" = "Wildfire.smoke",
-#          "Nitrogen dioxide (No2)" = "Nitrogen.dioxide",
-#          "Sulfur dioxide (So2)" = "Sulfur.dioxide",
-#          "Carbon monoxide (CO)" = "Carbon.monoxide",
-#          "Population density" = "Population.density"
-#   )
 sociodemographics <- c("Population (total)" = "Total.Population")
 
 racev <- c("Non-Hispanic White (percentage)" = "Percent.White.NonHispanic",
@@ -242,58 +153,35 @@ socialenv <- c("Food insecurity" = "Food.Insecurity",
 health_outcomes <- df_vars %>% 
   dplyr::select(c(21:32)) 
 
-health_outcomes_inp <- health_outcomes %>% 
-  st_drop_geometry()
-
 health_behaviors <- df_vars %>% 
   dplyr::select(c(17:20)) 
-health_behaviors_inp <- health_outcomes %>% 
-  st_drop_geometry()
 
 health_prevention <- df_vars %>% 
   dplyr::select(c(10:16)) 
-health_prevention_inp <- health_prevention %>% 
-  st_drop_geometry()
 
 natural_env <- df_vars %>%
   dplyr::select(c(75, 76, 88:92)) 
-natural_env_inp <- natural_env %>% 
-  st_drop_geometry()
 
 air_pol <- df_vars %>% 
   dplyr::select(c(1, 93:96)) 
-air_pol_inp <- air_pol %>% 
-  st_drop_geometry()
 
 built_env <- df_vars %>%
   dplyr::select(c(2:3, 74, 77, 78, 79)) 
-built_env_inp <- built_env %>% 
-  st_drop_geometry()
 
 sociodemo <- df_vars %>% 
   dplyr::select(c(52)) 
-sociodemo_inp <- sociodemo %>% 
-  st_drop_geometry()
 
 sex <- df_vars %>%
   dplyr::select(c(53, 54))
-sex_inp <- sex %>%
-  st_drop_geometry()
 
 race <- df_vars %>%
   dplyr::select(c(33:47))
-race_inp <- race %>%
-  st_drop_geometry()
 
 age <- df_vars %>%
   dplyr::select(c(55:72))
-age_inp <- df_vars %>%
-  st_drop_geometry()
 
 social_env <- df_vars %>% 
   dplyr::select(c(4:9, 73:74, 81:86)) 
-social_env_inp <- social_env %>% 
-  st_drop_geometry()
 
 food_env <- food %>% 
   dplyr::select(c(11:50)) 
@@ -534,8 +422,7 @@ server <- function(input, output, session) {
          "Asthma.among.Adults", "High.Blood.Pressure.among.Adults", "High.Blood.Pressure.among.Adults",
          "Cancer.or.Melanoma.among.Adults", "High.Cholesterol.among.Screened.Adults", "COPD.among.Adults",
          "Coronary.Heart.Disease.among.Adults", "Depression.among.Adults", "Diagnosed.Diabetes.among.Adults",
-         "Obesity.among.Adults", "All.Teeth.Lost.amond.Adults.65.and.older", "Stroke.amond.Adults",
-         names(air_pol_inp)) # CHANGE TO AMONG IN PROCESSING
+         "Obesity.among.Adults", "All.Teeth.Lost.among.Adults.65.and.older", "Stroke.among.Adults") 
   n <- c("Nighttime.Radiance", names(sociodemo_inp))
   
   # palette helper function
