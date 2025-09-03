82125b0f79c4a021e0766bfe97132faf7e32315b
Malia Cortez <mookiethompson17@gmail.com>
2025-09-03 10:15:18 -0700

    modify print options, toggle switches, cluster options

--- app.R
+++ app.R
@@ -284,11 +284,11 @@ categories <- accordion(
   ),
   accordion_panel(
     "Options", icon = bs_icon("gear"),
-    input_switch("showbounds", "Show Tract Boundaries", value = TRUE),
-    input_switch("showcounties", "Show County Boundaries", value = FALSE),
-    input_switch("showcities", "Show City Boundaries", value = FALSE),
-    input_switch("showchart", "Show Graph", value = FALSE),
-    fileInput("upload", "Upload a Shapefile"),
+    input_switch("showbounds", "Show tract boundaries", value = TRUE),
+    input_switch("showcounties", "Show county boundaries", value = FALSE),
+    input_switch("showcities", "Show city boundaries", value = FALSE),
+    input_switch("showchart", "Show graph", value = FALSE),
+    fileInput("upload", "Upload a shapefile"),
     downloadButton("download", "Download data")
   )
   
@@ -327,7 +327,8 @@ ui <- page_navbar(
                                 width = "400px"),
               reactableOutput("table")
             )),
-  nav_panel("Documentation"),
+  nav_panel("Documentation",
+            h2("Version History")),
   nav_panel("Contact us",
             h3("Questions or comments?"),
             a("geoexmap@fredhutch.org", href = "mailto:geoexmap@fredhutch.org")),
@@ -459,6 +460,8 @@ server <- function(input, output, session) {
     
     # reset switches if needed
     update_switch("transit", value = FALSE)
+    update_switch("showcounties", value = FALSE)
+    update_switch("showcities", value = FALSE)
     
     # clear the map
     leafletProxy("geoexmap") %>%
@@ -509,7 +512,8 @@ server <- function(input, output, session) {
       plot_ly(data = plotly.dat, x = plotly.dat[,1]) %>% 
         layout(
           plot_bgcolor = '#e5ecf6',
-          xaxis = list(title = names(plotly.dat)[1]))
+          xaxis = list(title = names(plotly.dat)[1])) %>% 
+        config()
     } else if (ncol(plotly.dat) == 2) {
       plot_ly(data = plotly.dat, type = "scatter", x = plotly.dat[,1], y = plotly.dat[,2],
               text = ~paste0("<b>", gsub("\\.", " ", names(plotly.dat)[1]), ": ", round(plotly.dat[,1], digits = 2),
@@ -517,7 +521,8 @@ server <- function(input, output, session) {
         layout(
           plot_bgcolor = '#e5ecf6',
           xaxis = list(title = gsub("\\.", " ", names(plotly.dat)[1])),
-          yaxis = list(title = gsub("\\.", " ", names(plotly.dat)[2])))
+          yaxis = list(title = gsub("\\.", " ", names(plotly.dat)[2]))) %>% 
+        config(scrollZoom = TRUE)
     } else if (ncol(plotly.dat) == 3) {
       plot_ly(data = plotly.dat, x = plotly.dat[,1], y = plotly.dat[,2], z = plotly.dat[,3],
               text = ~paste0("<b>", gsub("\\.", " ", names(plotly.dat)[1]), ": ", round(plotly.dat[,1], digits = 2),
@@ -525,7 +530,8 @@ server <- function(input, output, session) {
                              "<br>", gsub("\\.", " ", names(plotly.dat)[3]), ": ", round(plotly.dat[,3], digits = 2))) %>%
         layout(scene = list(xaxis = list(title = gsub("\\.", " ", names(plotly.dat)[1])),
                             yaxis = list(title = gsub("\\.", " ", names(plotly.dat)[2])),
-                            zaxis = list(title = gsub("\\.", " ", names(plotly.dat)[3]))))
+                            zaxis = list(title = gsub("\\.", " ", names(plotly.dat)[3])))) %>% 
+        config()
     } else {
       
     }
@@ -693,7 +699,7 @@ server <- function(input, output, session) {
         panel_html <- create_variable_panel(current_vars)
         proxy <- proxy %>% addControl(
             html = panel_html,
-            position = "topright",
+            position = "bottomright",
             layerId = "variable_panel"
           ) 
       }
@@ -708,7 +714,7 @@ server <- function(input, output, session) {
   <path d="M15 8a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1V2.64c0-1.188-.845-2.232-2.064-2.372A44 44 0 0 0 8 0C5.9 0 4.208.136 3.064.268 1.845.408 1 1.452 1 2.64V4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v3.5c0 .818.393 1.544 1 2v2a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5V14h6v1.5a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-2c.607-.456 1-1.182 1-2zM8 1c2.056 0 3.71.134 4.822.261.676.078 1.178.66 1.178 1.379v8.86a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 11.5V2.64c0-.72.502-1.301 1.178-1.379A43 43 0 0 1 8 1"/>
 </svg> Transit stops<br/>'
         
-        clusterOptions <- markerClusterOptions(spiderfyOnMaxZoom = FALSE)
+        clusterOptions <- markerClusterOptions(disableClusteringAtZoom = 14)
         
         proxy <- proxy %>% 
           addMarkers(
@@ -743,16 +749,6 @@ server <- function(input, output, session) {
           clearGroup(group = "cancer_programs")
       }
       
-      if (input$showcities) {
-        proxy <- proxy %>% 
-          addPolygons(data = city.bounds, stroke = TRUE, weight = 1, color = "#57737A")
-      }
-      
-      if (input$showcounties) {
-        proxy <- proxy %>% 
-          addPolygons(data = county.bounds, stroke = TRUE, weight = 2, color = "#85BDBF")
-      }
-      
       for (c in colnames(food_env_cols())) {
         pal <- geoex.palette(c)
         
@@ -783,6 +779,20 @@ server <- function(input, output, session) {
         proxy %>% 
           removeControl(layerId = "variable_panel")
       }
+      
+      if (input$showcounties) {
+        html_legend = ''
+        proxy <- proxy %>% 
+          addPolygons(data = county.bounds, stroke = TRUE, weight = 2, color = "#85BDBF", fill = FALSE) 
+      }
+      
+      if (input$showcities) {
+        proxy <- proxy %>% 
+          addPolygons(data = city.bounds, stroke = TRUE, weight = 2, color = "#57737A", fill = FALSE)
+      }
+      
+      proxy <- proxy %>% 
+        addEasyprint(options = easyprintOptions(filename = "geoexmap_output", hideControlContainer = FALSE))
       
     })  
   }) %>% 
